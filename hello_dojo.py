3987b68f149e159934e0ed247c121e4643aab27f
Bell Codo <bellcodo@gmail.com>
2016-01-15 16:54:33 -0800

    Create hello_dojo.py

---
+++ hello_dojo.py
@@ -0,0 +1,19 @@
+# Delete the beginning program!
+
+# We are going to write our first "program"
+# This is a python interpreter on the web
+# You write code in this window and it runs in
+#    ->>>>> that one
+
+# Write a multi-line string in python 
+# explaning why you decided to come
+
+# Example
+print """
+My name is Michael Schober. I have a son who is 8 years old. 
+He really likes to code, and I believe code dojo is a 
+great place to write code with peers in the community.
+
+I hope this dojo will grow to be a place where young people
+and old can come together near Bellevue and learn to code.
+"""
